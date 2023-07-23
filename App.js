a4bdafe2b12d5f0e0dc5582c0c8f65efff53aee6
ompranav <ompranav1993@gmail.com>
2023-07-23 23:55:26 +0530

    episode 1 by configuring username and email

--- App.js
+++ App.js
@@ -5,4 +5,4 @@ React.createElement("div",{id:"child"},React.createElement("h1",{},"I am h1 tag 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(heading) ;
