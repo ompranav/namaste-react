d778ecb03813a9fe35093a036ee845a2773cc8b9
ompranav <ompranav1993@gmail.com>
2023-07-24 19:07:12 +0530

    till writing React elements using jsx

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-const heading = React.createElement
-("div",{id :"parent"},
-React.createElement("div",{id:"child"},React.createElement("h1",{},"I am h1 tag ")));
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 
-
-
+const heading = React.createElement("h1", {id:"heading" }, "Namaste React");
+const jsxHeading =<h1 id="heading"> Namaste React using JSX </h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading) ;
+root.render(jsxHeading);
