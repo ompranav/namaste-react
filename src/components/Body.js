4d90081431daa634819791960f083e71c84f8954
ompranav <ompranav1993@gmail.com>
2023-08-18 12:14:48 +0530

    search

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,47 +4,54 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-
-  const [listOfRestaurant,setlistOfRestaurant] = useState ([]);
-  useEffect (()=>{
+  const [listOfRestaurant, setlistOfRestaurant] = useState([]);
+  useEffect(() => {
     fetchData();
-  } , []);
+  }, []);
 
-  const fetchData = async() => {
-    const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=12.9351929&lng=77.62448069999999");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/mapi/homepage/getCards?lat=12.9351929&lng=77.62448069999999"
+    );
 
     const json = await data.json();
     console.log(json);
-    setlistOfRestaurant (json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
-
-  }
-
-  if (listOfRestaurant.length===0) {
-    return <Shimmer />
-  }
- 
-
-    return (
-        <div className="body">
-            <div className="filter">
-                <button className="filter-btn" 
-                    onClick={()=> 
-                                {   const filteredList = listOfRestaurant.filter((res) => res.info.avgRating > 4.1 );
-                                    setlistOfRestaurant(filteredList);
-                                    
-                                }
-                            }>
-                    Top Rated Restaurants
-                    
-                </button>
-            </div>
-            <div className="res-container">
-              {listOfRestaurant.map((restaurant) => (<RestaurantCard key = {restaurant.info.id} resData ={restaurant}/>))};
-               
-
-            </div>
-        </div>
+    setlistOfRestaurant(
+      json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle
+        ?.restaurants
     );
+  };
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+            <input type="text" className="search-box" />
+            <button>Search</button>
+
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4.1
+            );
+            setlistOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+        ;
+      </div>
+    </div>
+  );
 };
 
 export default Body;
