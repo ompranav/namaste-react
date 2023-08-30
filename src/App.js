4968d806113e58870ee5238561d0dd3f38991ecd
ompranav <ompranav1993@gmail.com>
2023-08-30 23:31:44 +0530

    implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { lazy , Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+
 import { createBrowserRouter } from "react-router-dom";
 import About from "./components/About";
 import { RouterProvider } from "react-router-dom";
@@ -10,8 +11,13 @@ import Error from "./components/Error";
 import { Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-const jsxHeading = <h1 id="heading"> Namaste React using JSX </h1>;
+
+// we wont import like this -  import Grocery from "./components/Grocery";---instead will use lazy loading -
+
+//const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+//const jsxHeading = <h1 id="heading"> Namaste React using JSX </h1>;
+
+const Grocery = lazy(()=>import("./components/Grocery"));
 
 const AppLayout = () => {
   return (
@@ -40,12 +46,17 @@ const appRouter = createBrowserRouter ([
         element:<Contact/> 
       }, 
       {
+        path:"/grocery",
+        element: ( 
+        <Suspense fallback={<h1>Loading ....</h1>}>
+          <Grocery /> 
+        </Suspense> 
+        ),
+      }, 
+      {
         path:"/restaurant/:resId",
         element:<RestaurantMenu/> 
       }
-
-
-
     ],
     errorElement:<Error />
   },
---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+const Grocery = () => {
+    return (
+        <h1>This component is built to implement the concept of lazy loading </h1>
+       
+    )
+
+}
+
+export default Grocery;
